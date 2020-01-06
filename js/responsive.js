a647e473494f8911a32fc92cc80299b06a92b287
Julif <julif-@live.com>
2020-01-06 00:20:32 -0300

    [v 0.10.1]

--- js/responsive.js
+++ js/responsive.js
@@ -2,9 +2,9 @@ var plataform;
 responsive();
 
 function responsive(){
-  if ( window.innerWidth < 480 ) { plataform=" smartphone portrait"; }
-  if ( window.innerWidth < 480 && window.innerHeight < window.innerWidth) { plataform="smartphone landsape"; }
-  if ( window.innerWidth > 480 && window.innerWidth < 1024 ) { plataform="tablet"; }
+  if ( window.innerWidth < 680 ) { plataform=" smartphone portrait"; }
+  if ( window.innerWidth < 680 && window.innerHeight < window.innerWidth) { plataform="smartphone landsape"; }
+  if ( window.innerWidth > 680 && window.innerWidth < 1024 ) { plataform="tablet"; }
   if ( window.innerWidth > 1024 ) { plataform="desktop"; }
 
 
